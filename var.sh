897e4a707de38d79911910d9819ca905ae9cf403
root <root@libao.21v.com>
2018-01-20 12:27:26 +0800

    mend

--- var.sh
+++ var.sh
@@ -3,6 +3,18 @@
 #by authors libao
 
 A=123
+name=libao
 
+echo "my name is $name"
 echo "$A"
+echo $UID
+echo $PWD
 
+echo "####################"
+
+echo $0
+echo $1 $2
+
+echo "This is \$? is $?"
+echo "This is \$* is $*"
+echo "This is \$# is $#"
