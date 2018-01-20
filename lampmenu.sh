dde82106347348c5b2e94f9e6a31f2c6cfff8375
baolijn <libao_fighting@163.com>
2018-01-20 12:48:59 +0800

    add lampmenu.sh

---
+++ lampmenu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#by anthor libao 2018
+
+echo -e "\033[32mPlease select Menu follow:\033[1m "
+
+echo "1) install apache server"
+
+echo "2) install MySQL server"
+echo "3) install PHP server"
+echo "4) config LAMP WEB."
+
+echo "-------------------------"
