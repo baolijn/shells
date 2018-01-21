93ed38be4a302661210d1b96047fb0e8b231e6ef
baolijn <libao_fighting@163.com>
2018-01-21 09:37:29 +0800

    add until.sh

--- auto_lamp.sh
+++ auto_lamp.sh
@@ -8,6 +8,12 @@ H_FILE_DIR=httpd-2.2.34
 H_URL=http://mirrors.cnnic.cn/apache/httpd
 H_PREFIX=/usr/local/apache2
 
+#MySQL define path variale
+M_FILES=mysql-5.5.20.tar.gz
+M_FILES_DIR=mysql-5.5.20
+M_URL=http://down1.chinaunix.net/distfiles/
+M_PREFIX=/usr/local/mysql
+
 if [ -z "$1" ];then
 	echo -e "\033[32mPlease select follow install menu:033[0m"
 	echo -e "\033[32m1)Compile and install Apache server033[0m"
@@ -30,4 +36,44 @@ if [ $1 -eq 1 ];then
 		echo -e "\033[32mThe $H_FILE_DIR server install failed!033[0m"
 		exit
 	fi
+fi
+
+#Install Mysql DB server 
+if [[ "$1" -eq "2" ]];then
+
+        wget -c $M_URL/$M_FILES &&  tar -xzvf $M_FILES && cd $M_FILES_DIR &&yum install cmake -y ;cmake . -DCMAKE_INSTALL_PREFIX=$M_PREFIX \
+-DMYSQL_UNIX_ADDR=/tmp/mysql.sock \
+-DMYSQL_DATADIR=/data/mysql \
+-DSYSCONFDIR=/etc \
+-DMYSQL_USER=mysql \
+-DMYSQL_TCP_PORT=3306 \
+-DWITH_XTRADB_STORAGE_ENGINE=1 \
+-DWITH_INNOBASE_STORAGE_ENGINE=1 \
+-DWITH_PARTITION_STORAGE_ENGINE=1 \
+-DWITH_BLACKHOLE_STORAGE_ENGINE=1 \
+-DWITH_MYISAM_STORAGE_ENGINE=1 \
+-DWITH_READLINE=1 \
+-DENABLED_LOCAL_INFILE=1 \
+-DWITH_EXTRA_CHARSETS=1 \
+-DDEFAULT_CHARSET=utf8 \
+-DDEFAULT_COLLATION=utf8_general_ci \
+-DEXTRA_CHARSETS=all \
+-DWITH_BIG_TABLES=1 \
+-DWITH_DEBUG=0
+make && make install
+
+/bin/cp support-files/my-small.cnf /etc/my.conf
+/bin/cp support-files/mysql.server /etc/init.d/mysqld
+chmod +x /etc/init.d/mysqld
+chkconfig --add mysqld
+chkconfig mysqld on
+
+        if [ $? -eq 0 ];then
+                make && make install
+                echo -e "\n\033[32m-----------------------------------------------\033[0m"
+                echo -e "\033[32mThe $M_FILES_DIR Server Install Success !\033[0m"
+        else
+                echo -e "\033[32mThe $M_FILES_DIR Make or Make install ERROR,Please Check......"
+                exit 0
+        fi
 fi
--- while.sh
+++ while.sh
@@ -2,7 +2,8 @@
 
 i=0
 
-while [[ $i -lt 10 ]]
+#while [[ $i -lt 10 ]]
+while (( $i < 10 ))
 
 do
 	echo "The num is $i"
